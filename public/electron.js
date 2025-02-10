59b07c0c600b46d66145b97bfe2ef46e2fd2216f
blocmatrix <blocmatrix>
2025-02-10 18:18:39 +0530

    changes done

---
+++ public/electron.js
@@ -0,0 +1,31 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+
+  mainWindow.loadURL("http://localhost:3000"); // Or your app URL
+}
+
+app.whenReady().then(() => {
+  createWindow();
+
+  app.on("activate", () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on("window-all-closed", () => {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
